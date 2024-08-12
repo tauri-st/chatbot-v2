5aecb2d5963dcab6782f218e19aa07ace1cc4183
tauri_s <tauristclaire@gmail.com>
2024-08-12 15:02:09 -0700

    Pseudocode in the form of todos

--- chatbot.py
+++ chatbot.py
@@ -19,3 +19,18 @@ def get_api_chat_response_message(model, messages):
     return response_content
 
 model = "gpt-3.5-turbo"
+
+#TODO: create an array that will store the chat history
+
+#TODO: Create a while loop to manage the conversation lifecycle (i.e. keep the conversation running until the user chooses to terminate it) 
+#TODO: while the conversation is running:
+#TODO: prompt the user for input
+	#TODO: if the user types “exit”, stop the loop
+	#TODO: add the user’s input in the chat history
+	#TODO: make the API call
+	#TODO: extract the message content and display it to the user
+    #TODO: Use the assistant role to give the chatbot conversation context
+	    #add the message content to the chat history
+	#*(now the loop starts over!)
+  
+  #TODO: Greet your user and ask for their name to personalize the conversation
